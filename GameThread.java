f62b61ecf6eb0be36fbe8ec1df0b6025c8eeb168
micnubinub <lindelwencube.ln@gmail.com>
2015-01-04 03:36:08 +1000

    Files working

---
+++ GameThread.java
@@ -0,0 +1,58 @@
+package tbs.jumpsnew;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+    final SurfaceHolder surfaceHolder;
+    final Paint backgroundPaint;
+    final long DELAY = 1;
+    long sleepTime;
+    long lastUpdate = System.currentTimeMillis();
+    boolean isRunning;
+
+    public GameThread(SurfaceHolder surfaceHolder, Context context) {
+        this.surfaceHolder = surfaceHolder;
+        this.backgroundPaint = new Paint();
+        this.backgroundPaint.setARGB(255, 40, 40, 40);
+        this.isRunning = true;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            int delta = (int) (System.currentTimeMillis() - lastUpdate);
+            GameValues.SPEED_FACTOR = (int) ((GameValues.SPEED_FACTOR_ORIGINAL * GameValues.SPEED_BONUS) * delta);
+            if (GameValues.SPEED_FACTOR < 1)
+                GameValues.SPEED_FACTOR = 1;
+            GameValues.PLAYER_JUMP_SPEED = (GameValues.SPEED_FACTOR * GameValues.PLAYER_JUMP_SPEED_MULT);
+            lastUpdate = System.currentTimeMillis();
+            Game.update();
+
+            Canvas canvas = surfaceHolder.lockCanvas(null);
+            if (canvas != null) {
+                synchronized (surfaceHolder) {
+                    canvas.drawRect(0, 0, canvas.getWidth(),
+                            canvas.getHeight(), backgroundPaint);
+                    Game.draw(canvas);
+                }
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public boolean IsRunning() {
+        return isRunning;
+    }
+
+    public void SetIsRunning(boolean state) {
+        isRunning = state;
+    }
+}
